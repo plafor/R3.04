bf6d5ae49c9884a6db9466124d2f3ab7caecdd9a
Pierre Laforcade <pierre.laforcade@univ-lemans.fr>
2018-07-03 19:01:22 +0200

    add integration tests for version 2

---
+++ TD-guitar/Code-to-complete/src/exo2/searchTI.java
@@ -0,0 +1,60 @@
+package exo2;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.*;
+
+
+public class searchTI {
+
+	private static Inventory inventory;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception { 
+		inventory = new Inventory();
+	    FindGuitarTester.initializeInventory(inventory);
+	}
+
+	@Test
+	public void testSpecificGuitar() {
+	    Guitar whatCustomerWant = new Guitar("", 0, Builder.FENDER, "Stratocastor", 
+	                                      Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+	    List matchingGuitars = inventory.search(whatCustomerWant);
+	    
+	    Assert.assertNotNull(matchingGuitars);
+	    Assert.assertEquals(2, matchingGuitars.size());    
+	}
+	
+	@Test
+	public void testElectricGuitar() {
+	    Guitar whatCustomerWant = new Guitar("", 0, null, "", 
+	                                      Type.ELECTRIC, null, null);
+	    List matchingGuitars = inventory.search(whatCustomerWant);
+	    
+	    Assert.assertNotNull(matchingGuitars);
+	    Assert.assertEquals(5, matchingGuitars.size());    
+	}
+	
+	@Test
+	public void testAcousticGuitar() {
+	    Guitar whatCustomerWant = new Guitar("", 0, null, null, 
+	                                      Type.ACOUSTIC, null, null);
+	    List matchingGuitars = inventory.search(whatCustomerWant);
+	    
+	    Assert.assertNotNull(matchingGuitars);
+	    Assert.assertEquals(6, matchingGuitars.size());    
+	}
+	
+	@Test
+	public void testNoMachingGuitar() {
+	    Guitar whatCustomerWant = new Guitar("", 0, Builder.COLLINGS, null, 
+	                                      Type.ACOUSTIC, null, Wood.BRAZILIAN_ROSEWOOD);
+	    List matchingGuitars = inventory.search(whatCustomerWant);
+	    
+	    Assert.assertNotNull(matchingGuitars);
+	    Assert.assertTrue(matchingGuitars.isEmpty());    
+	}
+
+}
