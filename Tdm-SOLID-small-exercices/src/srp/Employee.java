92f21b0f55ab810843f0e0572a4e329d3f5166a1
Pierre Laforcade <pierre.laforcade@univ-lemans.fr>
2018-08-27 23:54:40 +0200

    add SRP exercise

---
+++ Tdm-SOLID-small-exercices/src/srp/Employee.java
@@ -0,0 +1,41 @@
+package srp;
+
+public class Employee {
+
+	public void calculatePay() {
+		//
+	}
+	
+	public void calculateTaxes() {
+		//
+	}
+	
+	public void writeToDisk() {
+		//
+	}
+	
+	public void readFromDisk() {
+		//
+	}
+	
+	public void createXML() {
+		//
+	}
+	
+	public void parseXML() {
+		//
+	}
+	
+	public void displayOnEmployeeReport() {
+		//
+	}
+	
+	public void displayOnPayrollReport() {
+		//
+	}
+	
+	public void displayOnTaxReport() {
+		//
+	}
+	
+}
